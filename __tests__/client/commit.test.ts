6519425622536c0d3481b0824b289f301c10ba1c
Zisis Maras <contact@zisismaras.me>
2019-07-12 21:06:06 +0300

    refactor tests

--- __tests__/client/commit.test.ts
+++ __tests__/client/commit.test.ts
@@ -2,171 +2,7 @@
 import "jest-extended";
 //tslint:enable
 import {PipeProc, IPipeProcClient} from "../../lib/client";
-import {v4 as uuid} from "uuid";
 import {getRandomPort} from "../utils/getRandomPort";
-
-// if (process.platform !== "win32") {
-//     describe("committing logs", function() {
-//         let client: IPipeProcClient;
-
-//         beforeEach(function(done) {
-//             client = PipeProc();
-
-//             client.spawn({memory: true, workers: 0, namespace: uuid()}).then(function() {
-//                 done();
-//             }).catch(function(err) {
-//                 done.fail(err);
-//             });
-//         });
-
-//         afterEach(function() {
-//             if (client.pipeProcNode) {
-//                 return client.shutdown();
-//             } else {
-//                 return Promise.resolve();
-//             }
-//         });
-
-//         it("should commit a simple log and return a logId with sequenceNumber 0 and a correct timestamp",
-//         function(done) {
-//             client.commit({
-//                 topic: "my_topic",
-//                 body: {
-//                     hello: 1
-//                 }
-//             }).then(function(logId) {
-//                 expect(logId).toBeString();
-//                 expect((<string>logId).split("-")[1]).toEqual("0");
-//                 expect(parseInt((<string>logId).split("-")[0])).toBeLessThanOrEqual(Date.now());
-//                 done();
-//             }).catch(function(err) {
-//                 done.fail(err);
-//             });
-//         });
-
-//         it("should have the sequence number increasing incrementally when multiple logs are committed", function(done) {
-//             client.commit([{
-//                 topic: "my_topic",
-//                 body: {
-//                     hello: 1
-//                 }
-//             }, {
-//                 topic: "my_topic",
-//                 body: {
-//                     hello: 2
-//                 }
-//             }]).then(function(logIds) {
-//                 expect(logIds).toBeArrayOfSize(2);
-//                 expect(logIds[0].split("-")[1]).toEqual("0");
-//                 expect(logIds[1].split("-")[1]).toEqual("1");
-//                 done();
-//             }).catch(function(err) {
-//                 done.fail(err);
-//             });
-//         });
-
-//         it("should have the same and correct timestamps when multiple logs are committed", function(done) {
-//             client.commit([{
-//                 topic: "my_topic",
-//                 body: {
-//                     hello: 1
-//                 }
-//             }, {
-//                 topic: "my_topic",
-//                 body: {
-//                     hello: 2
-//                 }
-//             }]).then(function(logIds) {
-//                 expect(logIds).toBeArrayOfSize(2);
-//                 const ts1 = parseInt(logIds[0].split("-")[0]);
-//                 const ts2 = parseInt(logIds[1].split("-")[0]);
-//                 expect(ts1).toEqual(ts2);
-//                 expect(ts1).toBeLessThanOrEqual(Date.now());
-//                 expect(ts2).toBeLessThanOrEqual(Date.now());
-//                 done();
-//             }).catch(function(err) {
-//                 done.fail(err);
-//             });
-//         });
-
-//         it("should have sequenceNumbers begin at zero if the 2 logs are committed to different topics", function(done) {
-//             client.commit([{
-//                 topic: "my_topic",
-//                 body: {
-//                     hello: 1
-//                 }
-//             }, {
-//                 topic: "my_topic_2",
-//                 body: {
-//                     hello: 2
-//                 }
-//             }]).then(function(logIds) {
-//                 expect(logIds).toBeArrayOfSize(2);
-//                 expect(logIds[0].split("-")[1]).toEqual("0");
-//                 expect(logIds[1].split("-")[1]).toEqual("0");
-//                 done();
-//             }).catch(function(err) {
-//                 done.fail(err);
-//             });
-//         });
-
-//         it("should return an error if the topic name is invalid", function(done) {
-//             client.commit({
-//                 topic: "an invalid topic",
-//                 body: {
-//                     hello: 1
-//                 }
-//             }).then(function(logId) {
-//                 done.fail("it should not accept invalid topic names");
-//             }).catch(function(err) {
-//                 expect(err).toBeInstanceOf(Error);
-//                 expect(err.message).toEqual("invalid_topic_format");
-//                 done();
-//             });
-//         });
-
-//         it("should return an error if the log body is not an object", function(done) {
-//             //@ts-ignore
-//             client.commit({
-//                 topic: "my_topic",
-//                 body: 123
-//             }).then(function(logId) {
-//                 done.fail("it should not accept invalid log bodies");
-//             }).catch(function(err) {
-//                 expect(err).toBeInstanceOf(Error);
-//                 expect(err.message).toEqual("invalid_log_format");
-//                 done();
-//             });
-//         });
-
-//         it("should return an error if the log body is missing", function(done) {
-//             //@ts-ignore
-//             client.commit({
-//                 topic: "my_topic"
-//             }).then(function(logId) {
-//                 done.fail("it should not accept invalid log bodies");
-//             }).catch(function(err) {
-//                 expect(err).toBeInstanceOf(Error);
-//                 expect(err.message).toEqual("invalid_log_format");
-//                 done();
-//             });
-//         });
-
-//         it("should return an error if the log body is null", function(done) {
-//             //@ts-ignore
-//             client.commit({
-//                 topic: "my_topic",
-//                 body: null
-//             }).then(function(logId) {
-//                 done.fail("it should not accept invalid log bodies");
-//             }).catch(function(err) {
-//                 expect(err).toBeInstanceOf(Error);
-//                 expect(err.message).toEqual("invalid_log_format");
-//                 done();
-//             });
-//         });
-//     });
-// }
 
 describe("committing logs with TCP", function() {
     let client: IPipeProcClient;
@@ -181,32 +17,26 @@ describe("committing logs with TCP", function() {
         });
     });
 
-    afterEach(function() {
-        if (client.pipeProcNode) {
-            return client.shutdown();
-        } else {
-            return Promise.resolve();
-        }
+    afterEach(async function() {
+        try {
+            await client.shutdown();
+        } catch (_e) {}
     });
 
-    it("should commit a simple log and return a logId with sequenceNumber 0 and a correct timestamp", function(done) {
-        client.commit({
+    it("should commit a simple log and return a logId with sequenceNumber 0 and a correct timestamp", async function() {
+        const logId = await client.commit({
             topic: "my_topic",
             body: {
                 hello: 1
             }
-        }).then(function(logId) {
-            expect(logId).toBeString();
-            expect((<string>logId).split("-")[1]).toEqual("0");
-            expect(parseInt((<string>logId).split("-")[0])).toBeLessThanOrEqual(Date.now());
-            done();
-        }).catch(function(err) {
-            done.fail(err);
         });
+        expect(logId).toBeString();
+        expect((<string>logId).split("-")[1]).toEqual("0");
+        expect(parseInt((<string>logId).split("-")[0])).toBeLessThanOrEqual(Date.now());
     });
 
-    it("should have the sequence number increasing incrementally when multiple logs are committed", function(done) {
-        client.commit([{
+    it("should have the sequence number increasing incrementally when multiple logs are committed", async function() {
+        const logIds = await client.commit([{
             topic: "my_topic",
             body: {
                 hello: 1
@@ -216,18 +46,14 @@ describe("committing logs with TCP", function() {
             body: {
                 hello: 2
             }
-        }]).then(function(logIds) {
-            expect(logIds).toBeArrayOfSize(2);
-            expect(logIds[0].split("-")[1]).toEqual("0");
-            expect(logIds[1].split("-")[1]).toEqual("1");
-            done();
-        }).catch(function(err) {
-            done.fail(err);
-        });
+        }]);
+        expect(logIds).toBeArrayOfSize(2);
+        expect(logIds[0].split("-")[1]).toEqual("0");
+        expect(logIds[1].split("-")[1]).toEqual("1");
     });
 
-    it("should have the same and correct timestamps when multiple logs are committed", function(done) {
-        client.commit([{
+    it("should have the same and correct timestamps when multiple logs are committed", async function() {
+        const logIds = await client.commit([{
             topic: "my_topic",
             body: {
                 hello: 1
@@ -237,21 +63,17 @@ describe("committing logs with TCP", function() {
             body: {
                 hello: 2
             }
-        }]).then(function(logIds) {
-            expect(logIds).toBeArrayOfSize(2);
-            const ts1 = parseInt(logIds[0].split("-")[0]);
-            const ts2 = parseInt(logIds[1].split("-")[0]);
-            expect(ts1).toEqual(ts2);
-            expect(ts1).toBeLessThanOrEqual(Date.now());
-            expect(ts2).toBeLessThanOrEqual(Date.now());
-            done();
-        }).catch(function(err) {
-            done.fail(err);
-        });
+        }]);
+        expect(logIds).toBeArrayOfSize(2);
+        const ts1 = parseInt(logIds[0].split("-")[0]);
+        const ts2 = parseInt(logIds[1].split("-")[0]);
+        expect(ts1).toEqual(ts2);
+        expect(ts1).toBeLessThanOrEqual(Date.now());
+        expect(ts2).toBeLessThanOrEqual(Date.now());
     });
 
-    it("should have sequenceNumbers begin at zero if the 2 logs are committed to different topics", function(done) {
-        client.commit([{
+    it("should have sequenceNumbers begin at zero if the 2 logs are committed to different topics", async function() {
+        const logIds = await client.commit([{
             topic: "my_topic",
             body: {
                 hello: 1
@@ -261,69 +83,49 @@ describe("committing logs with TCP", function() {
             body: {
                 hello: 2
             }
-        }]).then(function(logIds) {
-            expect(logIds).toBeArrayOfSize(2);
-            expect(logIds[0].split("-")[1]).toEqual("0");
-            expect(logIds[1].split("-")[1]).toEqual("0");
-            done();
-        }).catch(function(err) {
-            done.fail(err);
-        });
+        }]);
+        expect(logIds).toBeArrayOfSize(2);
+        expect(logIds[0].split("-")[1]).toEqual("0");
+        expect(logIds[1].split("-")[1]).toEqual("0");
     });
 
-    it("should return an error if the topic name is invalid", function(done) {
-        client.commit({
-            topic: "an invalid topic",
-            body: {
-                hello: 1
-            }
-        }).then(function(logId) {
-            done.fail("it should not accept invalid topic names");
-        }).catch(function(err) {
-            expect(err).toBeInstanceOf(Error);
-            expect(err.message).toEqual("invalid_topic_format");
-            done();
-        });
+    it("should return an error if the topic name is invalid", async function() {
+        expect((async function() {
+            await client.commit({
+                topic: "an invalid topic",
+                body: {
+                    hello: 1
+                }
+            });
+        })()).rejects.toThrowError("invalid_topic_format");
     });
 
-    it("should return an error if the log body is not an object", function(done) {
-        //@ts-ignore
-        client.commit({
-            topic: "my_topic",
-            body: 123
-        }).then(function(logId) {
-            done.fail("it should not accept invalid log bodies");
-        }).catch(function(err) {
-            expect(err).toBeInstanceOf(Error);
-            expect(err.message).toEqual("invalid_log_format");
-            done();
-        });
+    it("should return an error if the log body is not an object", async function() {
+        expect((async function() {
+            //@ts-ignore
+            await client.commit({
+                topic: "my_topic",
+                body: 123
+            });
+        })()).rejects.toThrowError("invalid_log_format");
     });
 
-    it("should return an error if the log body is missing", function(done) {
-        //@ts-ignore
-        client.commit({
-            topic: "my_topic"
-        }).then(function(logId) {
-            done.fail("it should not accept invalid log bodies");
-        }).catch(function(err) {
-            expect(err).toBeInstanceOf(Error);
-            expect(err.message).toEqual("invalid_log_format");
-            done();
-        });
+    it("should return an error if the log body is missing", async function() {
+        expect((async function() {
+            //@ts-ignore
+            await client.commit({
+                topic: "my_topic"
+            });
+        })()).rejects.toThrowError("invalid_log_format");
     });
 
-    it("should return an error if the log body is null", function(done) {
-        //@ts-ignore
-        client.commit({
-            topic: "my_topic",
-            body: null
-        }).then(function(logId) {
-            done.fail("it should not accept invalid log bodies");
-        }).catch(function(err) {
-            expect(err).toBeInstanceOf(Error);
-            expect(err.message).toEqual("invalid_log_format");
-            done();
-        });
+    it("should return an error if the log body is null", async function() {
+        expect((async function() {
+            //@ts-ignore
+            await client.commit({
+                topic: "my_topic",
+                body: null
+            });
+        })()).rejects.toThrowError("invalid_log_format");
     });
 });
--- __tests__/client/proccess.test.ts
+++ __tests__/client/proccess.test.ts
@@ -2,60 +2,19 @@
 import "jest-extended";
 //tslint:enable
 import {PipeProc, IPipeProcClient} from "../../lib/client";
-import {v4 as uuid} from "uuid";
 import {getRandomPort} from "../utils/getRandomPort";
 
-// if (process.platform !== "win32") {
-//     describe("spawning the node", function() {
-//         let client: IPipeProcClient;
-
-//         beforeEach(function() {
-//             client = PipeProc();
-//         });
-
-//         afterEach(function() {
-//             if (client.pipeProcNode) {
-//                 return client.shutdown();
-//             } else {
-//                 return Promise.resolve();
-//             }
-//         });
-
-//         it("should spawn correctly", function(done) {
-//             client.spawn({memory: true, workers: 0, namespace: uuid()}).then(function(status) {
-//                 expect(status).toEqual("spawned_and_connected");
-//                 done();
-//             });
-//         });
-
-//         it("should return a warning if spawned twice", function(done) {
-//             client.spawn({memory: true, workers: 0, namespace: uuid()}).then(function(status) {
-//                 expect(status).toEqual("spawned_and_connected");
-//                 client.spawn({memory: true}).then(function(status2) {
-//                     expect(status2).toEqual("node_already_active");
-//                     done();
-//                 }).catch(function(err) {
-//                     done.fail(err);
-//                 });
-//             }).catch(function(err) {
-//                 done.fail(err);
-//             });
-//         });
-//     });
-// }
-describe("spawning the node with TCP", function() {
+describe("spawning the node", function() {
     let client: IPipeProcClient;
 
     beforeEach(function() {
         client = PipeProc();
     });
 
-    afterEach(function() {
-        if (client.pipeProcNode) {
-            return client.shutdown();
-        } else {
-            return Promise.resolve();
-        }
+    afterEach(async function() {
+        try {
+            await client.shutdown();
+        } catch (_e) {}
     });
 
     it("should spawn correctly", async function() {
@@ -82,56 +41,7 @@ describe("spawning the node with TCP", function() {
     });
 });
 
-// if (process.platform !== "win32") {
-//     describe("connecting to an existing node", function() {
-//         let client: IPipeProcClient;
-//         let namespace: string;
-
-//         beforeEach(function(done) {
-//             client = PipeProc();
-//             namespace = uuid();
-//             client.spawn({memory: true, workers: 0, namespace: namespace}).then(function() {
-//                 done();
-//             }).catch(function(err) {
-//                 done.fail(err);
-//             });
-//         });
-
-//         afterEach(function() {
-//             if (client.pipeProcNode) {
-//                 return client.shutdown();
-//             } else {
-//                 return Promise.resolve();
-//             }
-//         });
-
-//         it("should be able to connect to an existing node", function(done) {
-//             const client2 = PipeProc();
-//             client2.connect({namespace: namespace}).then(function(status) {
-//                 expect(status).toEqual("connected");
-//                 done();
-//             });
-//         });
-
-//         it("should allow multiple clients to connect", async function() {
-//             const client2 = PipeProc();
-//             const client3 = PipeProc();
-//             const status = await client2.connect({namespace: namespace});
-//             expect(status).toEqual("connected");
-//             const status2 = await client3.connect({namespace: namespace});
-//             expect(status2).toEqual("connected");
-//         });
-
-//         it("should return a notice if we are already connected", function(done) {
-//             client.connect({namespace: namespace}).then(function(status) {
-//                 expect(status).toEqual("already_connected");
-//                 done();
-//             });
-//         });
-//     });
-// }
-
-describe("connecting to an existing node with TCP", function() {
+describe("connecting to an existing node", function() {
     let client: IPipeProcClient;
     let tcp;
 
@@ -149,12 +59,10 @@ describe("connecting to an existing node with TCP", function() {
         }
     });
 
-    it("should be able to connect to an existing node", function(done) {
+    it("should be able to connect to an existing node", async function() {
         const client2 = PipeProc();
-        client2.connect({tcp: tcp}).then(function(status) {
-            expect(status).toEqual("connected");
-            done();
-        });
+        const status = await client2.connect({tcp: tcp});
+        expect(status).toEqual("connected");
     });
 
     it("should allow multiple clients to connect", async function() {
@@ -166,45 +74,13 @@ describe("connecting to an existing node with TCP", function() {
         expect(status2).toEqual("connected");
     });
 
-    it("should return a notice if we are already connected", function(done) {
-        client.connect({tcp: tcp}).then(function(status) {
-            expect(status).toEqual("already_connected");
-            done();
-        });
+    it("should return a notice if we are already connected", async function() {
+        const status = await client.connect({tcp: tcp});
+        expect(status).toEqual("already_connected");
     });
 });
 
-// if (process.platform !== "win32") {
-//     describe("shutting down a node", function() {
-//         let client: IPipeProcClient;
-
-//         beforeEach(function() {
-//             client = PipeProc();
-//         });
-
-//         it("should be able to shutdown a node", function(done) {
-//             //spawn it first
-//             client.spawn({memory: true, workers: 1, namespace: uuid()}).then(function() {
-//                 client.shutdown().then(function(status) {
-//                     expect(status).toEqual("closed");
-//                     done();
-//                 });
-//             }).catch(function(err) {
-//                 done.fail(err);
-//             });
-//         });
-
-//         it("should raise an error if there is no active node", function(done) {
-//             client.shutdown().catch(function(err) {
-//                 expect(err).toBeInstanceOf(Error);
-//                 expect(err.message).toEqual("no_active_node");
-//                 done();
-//             });
-//         });
-//     });
-// }
-
-describe("shutting down a node with TCP", function() {
+describe("shutting down a node", function() {
     let client: IPipeProcClient;
 
     beforeEach(function() {
@@ -222,11 +98,10 @@ describe("shutting down a node with TCP", function() {
         expect(status).toEqual("closed");
     });
 
-    it("should raise an error if there is no active node", function(done) {
-        client.shutdown().catch(function(err) {
-            expect(err).toBeInstanceOf(Error);
-            expect(err.message).toEqual("no_active_node");
-            done();
-        });
+    it("should raise an error if there is no active node", async function() {
+        expect((async function() {
+            //@ts-ignore
+            await client.shutdown();
+        })()).rejects.toThrowError("no_active_node");
     });
 });
