a0fe6a82e54ad5deded35ae77013ca8e59168685
Zisis Maras <contact@zisismaras.me>
2019-07-12 19:58:12 +0300

    disable ipc tests

--- __tests__/client/proccess.test.ts
+++ __tests__/client/proccess.test.ts
@@ -5,44 +5,44 @@ import {PipeProc, IPipeProcClient} from "../../lib/client";
 import {v4 as uuid} from "uuid";
 import {getRandomPort} from "../utils/getRandomPort";
 
-if (process.platform !== "win32") {
-    describe("spawning the node", function() {
-        let client: IPipeProcClient;
+// if (process.platform !== "win32") {
+//     describe("spawning the node", function() {
+//         let client: IPipeProcClient;
 
-        beforeEach(function() {
-            client = PipeProc();
-        });
+//         beforeEach(function() {
+//             client = PipeProc();
+//         });
 
-        afterEach(function() {
-            if (client.pipeProcNode) {
-                return client.shutdown();
-            } else {
-                return Promise.resolve();
-            }
-        });
+//         afterEach(function() {
+//             if (client.pipeProcNode) {
+//                 return client.shutdown();
+//             } else {
+//                 return Promise.resolve();
+//             }
+//         });
 
-        it("should spawn correctly", function(done) {
-            client.spawn({memory: true, workers: 0, namespace: uuid()}).then(function(status) {
-                expect(status).toEqual("spawned_and_connected");
-                done();
-            });
-        });
+//         it("should spawn correctly", function(done) {
+//             client.spawn({memory: true, workers: 0, namespace: uuid()}).then(function(status) {
+//                 expect(status).toEqual("spawned_and_connected");
+//                 done();
+//             });
+//         });
 
-        it("should return a warning if spawned twice", function(done) {
-            client.spawn({memory: true, workers: 0, namespace: uuid()}).then(function(status) {
-                expect(status).toEqual("spawned_and_connected");
-                client.spawn({memory: true}).then(function(status2) {
-                    expect(status2).toEqual("node_already_active");
-                    done();
-                }).catch(function(err) {
-                    done.fail(err);
-                });
-            }).catch(function(err) {
-                done.fail(err);
-            });
-        });
-    });
-}
+//         it("should return a warning if spawned twice", function(done) {
+//             client.spawn({memory: true, workers: 0, namespace: uuid()}).then(function(status) {
+//                 expect(status).toEqual("spawned_and_connected");
+//                 client.spawn({memory: true}).then(function(status2) {
+//                     expect(status2).toEqual("node_already_active");
+//                     done();
+//                 }).catch(function(err) {
+//                     done.fail(err);
+//                 });
+//             }).catch(function(err) {
+//                 done.fail(err);
+//             });
+//         });
+//     });
+// }
 describe("spawning the node with TCP", function() {
     let client: IPipeProcClient;
 
@@ -82,54 +82,54 @@ describe("spawning the node with TCP", function() {
     });
 });
 
-if (process.platform !== "win32") {
-    describe("connecting to an existing node", function() {
-        let client: IPipeProcClient;
-        let namespace: string;
+// if (process.platform !== "win32") {
+//     describe("connecting to an existing node", function() {
+//         let client: IPipeProcClient;
+//         let namespace: string;
 
-        beforeEach(function(done) {
-            client = PipeProc();
-            namespace = uuid();
-            client.spawn({memory: true, workers: 0, namespace: namespace}).then(function() {
-                done();
-            }).catch(function(err) {
-                done.fail(err);
-            });
-        });
+//         beforeEach(function(done) {
+//             client = PipeProc();
+//             namespace = uuid();
+//             client.spawn({memory: true, workers: 0, namespace: namespace}).then(function() {
+//                 done();
+//             }).catch(function(err) {
+//                 done.fail(err);
+//             });
+//         });
 
-        afterEach(function() {
-            if (client.pipeProcNode) {
-                return client.shutdown();
-            } else {
-                return Promise.resolve();
-            }
-        });
+//         afterEach(function() {
+//             if (client.pipeProcNode) {
+//                 return client.shutdown();
+//             } else {
+//                 return Promise.resolve();
+//             }
+//         });
 
-        it("should be able to connect to an existing node", function(done) {
-            const client2 = PipeProc();
-            client2.connect({namespace: namespace}).then(function(status) {
-                expect(status).toEqual("connected");
-                done();
-            });
-        });
+//         it("should be able to connect to an existing node", function(done) {
+//             const client2 = PipeProc();
+//             client2.connect({namespace: namespace}).then(function(status) {
+//                 expect(status).toEqual("connected");
+//                 done();
+//             });
+//         });
 
-        it("should allow multiple clients to connect", async function() {
-            const client2 = PipeProc();
-            const client3 = PipeProc();
-            const status = await client2.connect({namespace: namespace});
-            expect(status).toEqual("connected");
-            const status2 = await client3.connect({namespace: namespace});
-            expect(status2).toEqual("connected");
-        });
+//         it("should allow multiple clients to connect", async function() {
+//             const client2 = PipeProc();
+//             const client3 = PipeProc();
+//             const status = await client2.connect({namespace: namespace});
+//             expect(status).toEqual("connected");
+//             const status2 = await client3.connect({namespace: namespace});
+//             expect(status2).toEqual("connected");
+//         });
 
-        it("should return a notice if we are already connected", function(done) {
-            client.connect({namespace: namespace}).then(function(status) {
-                expect(status).toEqual("already_connected");
-                done();
-            });
-        });
-    });
-}
+//         it("should return a notice if we are already connected", function(done) {
+//             client.connect({namespace: namespace}).then(function(status) {
+//                 expect(status).toEqual("already_connected");
+//                 done();
+//             });
+//         });
+//     });
+// }
 
 describe("connecting to an existing node with TCP", function() {
     let client: IPipeProcClient;
@@ -174,35 +174,35 @@ describe("connecting to an existing node with TCP", function() {
     });
 });
 
-if (process.platform !== "win32") {
-    describe("shutting down a node", function() {
-        let client: IPipeProcClient;
+// if (process.platform !== "win32") {
+//     describe("shutting down a node", function() {
+//         let client: IPipeProcClient;
 
-        beforeEach(function() {
-            client = PipeProc();
-        });
+//         beforeEach(function() {
+//             client = PipeProc();
+//         });
 
-        it("should be able to shutdown a node", function(done) {
-            //spawn it first
-            client.spawn({memory: true, workers: 1, namespace: uuid()}).then(function() {
-                client.shutdown().then(function(status) {
-                    expect(status).toEqual("closed");
-                    done();
-                });
-            }).catch(function(err) {
-                done.fail(err);
-            });
-        });
+//         it("should be able to shutdown a node", function(done) {
+//             //spawn it first
+//             client.spawn({memory: true, workers: 1, namespace: uuid()}).then(function() {
+//                 client.shutdown().then(function(status) {
+//                     expect(status).toEqual("closed");
+//                     done();
+//                 });
+//             }).catch(function(err) {
+//                 done.fail(err);
+//             });
+//         });
 
-        it("should raise an error if there is no active node", function(done) {
-            client.shutdown().catch(function(err) {
-                expect(err).toBeInstanceOf(Error);
-                expect(err.message).toEqual("no_active_node");
-                done();
-            });
-        });
-    });
-}
+//         it("should raise an error if there is no active node", function(done) {
+//             client.shutdown().catch(function(err) {
+//                 expect(err).toBeInstanceOf(Error);
+//                 expect(err.message).toEqual("no_active_node");
+//                 done();
+//             });
+//         });
+//     });
+// }
 
 describe("shutting down a node with TCP", function() {
     let client: IPipeProcClient;
--- __tests__/client/range.test.ts
+++ __tests__/client/range.test.ts
@@ -5,152 +5,152 @@ import {PipeProc, IPipeProcClient} from "../../lib/client";
 import {v4 as uuid} from "uuid";
 import {getRandomPort} from "../utils/getRandomPort";
 
-if (process.platform !== "win32") {
-    describe("using range", function() {
-        let testLogIds: string[];
-        let client: IPipeProcClient;
+// if (process.platform !== "win32") {
+//     describe("using range", function() {
+//         let testLogIds: string[];
+//         let client: IPipeProcClient;
 
-        beforeEach(function(done) {
-            client = PipeProc();
+//         beforeEach(function(done) {
+//             client = PipeProc();
 
-            client.spawn({memory: true, workers: 0, namespace: uuid()}).then(function() {
-                commitSomeLogs(client).then(function(logs) {
-                    testLogIds = logs;
-                    done();
-                }).catch(function(err) {
-                    done.fail(err);
-                });
-            }).catch(function(err) {
-                done.fail(err);
-            });
-        });
+//             client.spawn({memory: true, workers: 0, namespace: uuid()}).then(function() {
+//                 commitSomeLogs(client).then(function(logs) {
+//                     testLogIds = logs;
+//                     done();
+//                 }).catch(function(err) {
+//                     done.fail(err);
+//                 });
+//             }).catch(function(err) {
+//                 done.fail(err);
+//             });
+//         });
 
-        afterEach(function() {
-            if (client.pipeProcNode) {
-                return client.shutdown();
-            } else {
-                return Promise.resolve();
-            }
-        });
+//         afterEach(function() {
+//             if (client.pipeProcNode) {
+//                 return client.shutdown();
+//             } else {
+//                 return Promise.resolve();
+//             }
+//         });
 
-        it("should get the whole topic if no start and end are provided", function(done) {
-            client.range("my_topic").then(function(logs) {
-                expect(logs).toBeArrayOfSize(3);
-                done();
-            });
-        });
+//         it("should get the whole topic if no start and end are provided", function(done) {
+//             client.range("my_topic").then(function(logs) {
+//                 expect(logs).toBeArrayOfSize(3);
+//                 done();
+//             });
+//         });
 
-        it("should honor the limit option", function(done) {
-            client.range("my_topic", {limit: 1}).then(function(logs) {
-                expect(logs).toBeArrayOfSize(1);
-                done();
-            });
-        });
+//         it("should honor the limit option", function(done) {
+//             client.range("my_topic", {limit: 1}).then(function(logs) {
+//                 expect(logs).toBeArrayOfSize(1);
+//                 done();
+//             });
+//         });
 
-        it("should return the whole topic if limit is -1", function(done) {
-            client.range("my_topic", {limit: -1}).then(function(logs) {
-                expect(logs).toBeArrayOfSize(3);
-                done();
-            });
-        });
+//         it("should return the whole topic if limit is -1", function(done) {
+//             client.range("my_topic", {limit: -1}).then(function(logs) {
+//                 expect(logs).toBeArrayOfSize(3);
+//                 done();
+//             });
+//         });
 
-        it("should honor the start option", function(done) {
-            client.range("my_topic", {start: testLogIds[1]}).then(function(logs) {
-                expect(logs).toBeArrayOfSize(2);
-                expect(logs[0].id).toEqual(testLogIds[1]);
-                done();
-            });
-        });
+//         it("should honor the start option", function(done) {
+//             client.range("my_topic", {start: testLogIds[1]}).then(function(logs) {
+//                 expect(logs).toBeArrayOfSize(2);
+//                 expect(logs[0].id).toEqual(testLogIds[1]);
+//                 done();
+//             });
+//         });
 
-        it("should honor the end option", function(done) {
-            client.range("my_topic", {end: testLogIds[1]}).then(function(logs) {
-                expect(logs).toBeArrayOfSize(2);
-                expect(logs[1].id).toEqual(testLogIds[1]);
-                done();
-            });
-        });
+//         it("should honor the end option", function(done) {
+//             client.range("my_topic", {end: testLogIds[1]}).then(function(logs) {
+//                 expect(logs).toBeArrayOfSize(2);
+//                 expect(logs[1].id).toEqual(testLogIds[1]);
+//                 done();
+//             });
+//         });
 
-        it("should honor the start and end option", function(done) {
-            client.range("my_topic", {start: testLogIds[1], end: testLogIds[2]}).then(function(logs) {
-                expect(logs).toBeArrayOfSize(2);
-                expect(logs[0].id).toEqual(testLogIds[1]);
-                expect(logs[1].id).toEqual(testLogIds[2]);
-                done();
-            });
-        });
+//         it("should honor the start and end option", function(done) {
+//             client.range("my_topic", {start: testLogIds[1], end: testLogIds[2]}).then(function(logs) {
+//                 expect(logs).toBeArrayOfSize(2);
+//                 expect(logs[0].id).toEqual(testLogIds[1]);
+//                 expect(logs[1].id).toEqual(testLogIds[2]);
+//                 done();
+//             });
+//         });
 
-        it("should honor the exclusive option", function(done) {
-            client.range("my_topic", {
-                start: testLogIds[0],
-                end: testLogIds[2],
-                exclusive: true
-            }).then(function(logs) {
-                expect(logs).toBeArrayOfSize(1);
-                expect(logs[0].id).toEqual(testLogIds[1]);
-                done();
-            });
-        });
+//         it("should honor the exclusive option", function(done) {
+//             client.range("my_topic", {
+//                 start: testLogIds[0],
+//                 end: testLogIds[2],
+//                 exclusive: true
+//             }).then(function(logs) {
+//                 expect(logs).toBeArrayOfSize(1);
+//                 expect(logs[0].id).toEqual(testLogIds[1]);
+//                 done();
+//             });
+//         });
 
-        it("should accept a timestamp only", function(done) {
-            client.range("my_topic", {
-                start: testLogIds[0].split("-")[0]
-            }).then(function(logs) {
-                expect(logs).toBeArrayOfSize(3);
-                done();
-            });
-        });
+//         it("should accept a timestamp only", function(done) {
+//             client.range("my_topic", {
+//                 start: testLogIds[0].split("-")[0]
+//             }).then(function(logs) {
+//                 expect(logs).toBeArrayOfSize(3);
+//                 done();
+//             });
+//         });
 
-        it("should accept a sequenceNumber only", function(done) {
-            client.range("my_topic", {
-                start: `:${testLogIds[2].split("-")[1]}`
-            }).then(function(logs) {
-                expect(logs).toBeArrayOfSize(1);
-                done();
-            });
-        });
+//         it("should accept a sequenceNumber only", function(done) {
+//             client.range("my_topic", {
+//                 start: `:${testLogIds[2].split("-")[1]}`
+//             }).then(function(logs) {
+//                 expect(logs).toBeArrayOfSize(1);
+//                 done();
+//             });
+//         });
 
-        it("should return an error if the sequenceNumber(tone id) does not exist", function(done) {
-            client.range("my_topic", {
-                start: ":123"
-            }).catch(function(err) {
-                expect(err).toBeInstanceOf(Error);
-                expect(err.message).toEqual("invalid_tone_id_search");
-                done();
-            });
-        });
+//         it("should return an error if the sequenceNumber(tone id) does not exist", function(done) {
+//             client.range("my_topic", {
+//                 start: ":123"
+//             }).catch(function(err) {
+//                 expect(err).toBeInstanceOf(Error);
+//                 expect(err.message).toEqual("invalid_tone_id_search");
+//                 done();
+//             });
+//         });
 
-        it("should return an error if the start option is invalid", function(done) {
-            client.range("my_topic", {
-                start: "invalid"
-            }).catch(function(err) {
-                expect(err).toBeInstanceOf(Error);
-                expect(err.message).toEqual("invalid_range_offset");
-                done();
-            });
-        });
+//         it("should return an error if the start option is invalid", function(done) {
+//             client.range("my_topic", {
+//                 start: "invalid"
+//             }).catch(function(err) {
+//                 expect(err).toBeInstanceOf(Error);
+//                 expect(err.message).toEqual("invalid_range_offset");
+//                 done();
+//             });
+//         });
 
-        it("should return an error if the end option is invalid", function(done) {
-            client.range("my_topic", {
-                end: "invalid"
-            }).catch(function(err) {
-                expect(err).toBeInstanceOf(Error);
-                expect(err.message).toEqual("invalid_range_offset");
-                done();
-            });
-        });
+//         it("should return an error if the end option is invalid", function(done) {
+//             client.range("my_topic", {
+//                 end: "invalid"
+//             }).catch(function(err) {
+//                 expect(err).toBeInstanceOf(Error);
+//                 expect(err.message).toEqual("invalid_range_offset");
+//                 done();
+//             });
+//         });
 
-        it("should not return results if start > end", function(done) {
-            client.range("my_topic", {
-                start: testLogIds[1],
-                end: testLogIds[0]
-            }).then(function(logs) {
-                expect(logs).toBeArrayOfSize(0);
-                done();
-            });
-        });
+//         it("should not return results if start > end", function(done) {
+//             client.range("my_topic", {
+//                 start: testLogIds[1],
+//                 end: testLogIds[0]
+//             }).then(function(logs) {
+//                 expect(logs).toBeArrayOfSize(0);
+//                 done();
+//             });
+//         });
 
-    });
-}
+//     });
+// }
 
 describe("using range with TCP", function() {
     let testLogIds: string[];
